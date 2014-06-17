cf352fdf9e2f9c344feb1899c8c46ba6c9f36b26
marty <juliomartinphernandez@gmail.com>
2014-06-18 05:54:25 +0800

    Added Parse

--- app/src/main/java/com/martyawesome/ribbit/app/MainActivity.java
+++ app/src/main/java/com/martyawesome/ribbit/app/MainActivity.java
@@ -16,6 +16,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.parse.ParseAnalytics;
+
 import java.util.Locale;
 
 
@@ -40,6 +42,8 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ParseAnalytics.trackAppOpened(getIntent());
 
         Intent intent = new Intent(this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
